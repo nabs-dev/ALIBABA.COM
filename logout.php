e4df9521dbe5a54411e54b8ceffe848d39b0b7d5
nabs-dev <nabihakashif631@gmail.com>
2025-07-30 01:42:49 +0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+session_destroy();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logout</title>
+    <script>
+        window.location.href = 'login.php';
+    </script>
+</head>
+<body>
+</body>
+</html>
